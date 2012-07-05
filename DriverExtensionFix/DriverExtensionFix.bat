4bd8b71d90bca9af14292d5a685bb328c93962f7
n8felton <n8felton@gmail.com>
2012-07-05 11:21:39 -0400

    Adding DriverExtensionFix

---
+++ DriverExtensionFix/DriverExtensionFix.bat
@@ -0,0 +1,29 @@
+xcopy %~dp0*.dl_ %~dp0*.dll
+del %~dp0*.dl_
+xcopy %~dp0*.ex_ %~dp0*.exe
+del %~dp0*.ex_
+xcopy %~dp0*.sy_ %~dp0*.sys
+del %~dp0*.sy_
+xcopy %~dp0*.da_ %~dp0*.dat
+del %~dp0*.da_
+xcopy %~dp0*.ca_ %~dp0*.cap
+del %~dp0*.ca_
+xcopy %~dp0*.pr_ %~dp0*.prm
+del %~dp0*.pr_
+xcopy %~dp0*.xm_ %~dp0*.xml
+del %~dp0*.xm_
+xcopy %~dp0*.ch_ %~dp0*.chm
+del %~dp0*.ch_
+xcopy %~dp0*.lm_ %~dp0*.lmc
+del %~dp0*.lm_
+xcopy %~dp0*.cf_ %~dp0*.cfg
+del %~dp0*.cf_
+xcopy %~dp0*.vi_ %~dp0*.vif
+del %~dp0*.vi_
+xcopy %~dp0*.ic_ %~dp0*.icm
+del %~dp0*.ic_
+xcopy %~dp0*.km_ %~dp0*.kmd
+del %~dp0*.km_
+xcopy %~dp0*.bi_ %~dp0*.bin
+del %~dp0*.bi_
+pause
